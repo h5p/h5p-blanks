ad413be41567f64a2c0c18f9ded28614ab3b0fd4
Frode Petterson <frode.andre.petterson@gmail.com>
2017-06-27 09:37:32 +0200

    Add support for overall feedback
    HFP-1174

--- js/blanks.js
+++ js/blanks.js
@@ -53,8 +53,14 @@ H5P.Blanks = (function ($, Question) {
       questions: [
         "Oslo is the capital of *Norway*."
       ],
-      userAnswers: [],
-      score: "You got @score of @total points",
+      overallFeedback: [
+        {
+          'from': 0,
+          'to': 100,
+          'feedback': 'You got @score of @total points'
+        }
+      ],
+      userAnswers: [], // TODO This isn't in semantics?
       showSolutions: "Show solution",
       tryAgain: "Try again",
       checkAnswer: "Check",
@@ -709,7 +715,7 @@ H5P.Blanks = (function ($, Question) {
   Blanks.prototype.showEvaluation = function () {
     var maxScore = this.getMaxScore();
     var score = this.getScore();
-    var scoreText = this.params.score.replace('@score', score).replace('@total', maxScore);
+    var scoreText = H5P.Question.determineOverallFeedback(this.params.overallFeedback, score / maxScore).replace('@score', score).replace('@total', maxScore);
     this.setFeedback(scoreText, score, maxScore);
 
     if (score === maxScore) {
