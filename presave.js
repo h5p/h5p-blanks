d0327c492bf008afebc8129c35a32870620a293c
Thomas Horn Sivertsen <thomass@cerpus.com>
2018-03-27 16:20:35 +0200

    Added/modified calculation of max score

--- presave.js
+++ presave.js
@@ -1,28 +1,31 @@
 var H5PPresave = H5PPresave || {};
 
 H5PPresave['H5P.Blanks'] = function (content, finished) {
-    if (typeof content === "undefined" || !content.hasOwnProperty('questions') || !Array.isArray(content.questions) ) {
-        throw {
-            name: 'Invalid Fill in the blanks Error',
-            message: "Could not find expected semantics in content."
-        };
+  var presave = H5PEditor.Presave;
+
+  if (isContentInValid()) {
+    throw {
+      name: 'Invalid Fill in the blanks Error',
+      message: 'Could not find expected semantics in content.'
     }
+  }
 
-    var score = content.questions
-            .map(function (question) {
-                var pattern = /\*[^\*]+\*/g;
-                return question.match(pattern || []).length;
-            })
-            .reduce(function (previous, current) {
-                return previous + current;
-            }, 0);
+  var score = content.questions
+    .map(function (question) {
+      var pattern = /\*[^\*]+\*/g;
+      return question.match(pattern || []).length
+    })
+    .reduce(function (previous, current) {
+      return previous + current
+    }, 0);
 
-    if( isNaN(score) || score < 0){
-        throw {
-            name: 'InvalidMaxScore Error',
-            message: "Could not calculate the max score for this content. The max score is assumed to be 0. Contact your administrator if this isn’t correct."
-        };
-    }
+  presave.validateScore(score);
 
-    finished({maxScore: score});
+  if (finished) {
+    finished({maxScore: score})
+  }
+
+  function isContentInValid() {
+    return !presave.checkNestedRequirements(content, 'content.questions') || !Array.isArray(content.questions);
+  }
 };
