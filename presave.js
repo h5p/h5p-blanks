cb8f69ff5a231d2ea1cf280e61c46a678e3b127a
Thomas Horn Sivertsen <thomass@cerpus.com>
2018-03-28 11:44:54 +0200

    Added JSDoc

--- presave.js
+++ presave.js
@@ -1,5 +1,12 @@
 var H5PPresave = H5PPresave || {};
 
+/**
+ * Resolve the presave logic for the content type Fill in the Blanks
+ *
+ * @param {object} content
+ * @param finished
+ * @constructor
+ */
 H5PPresave['H5P.Blanks'] = function (content, finished) {
   var presave = H5PEditor.Presave;
 
@@ -25,6 +32,10 @@ H5PPresave['H5P.Blanks'] = function (content, finished) {
     finished({maxScore: score})
   }
 
+  /**
+   * Check if required parameters is present
+   * @return {boolean}
+   */
   function isContentInValid() {
     return !presave.checkNestedRequirements(content, 'content.questions') || !Array.isArray(content.questions);
   }
