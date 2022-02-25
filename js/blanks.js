ab08cb430699435c2a9af65b36a06a1894a2e7a4
Ravi Majithia <ravikiran.majithia@joubel.com>
2022-02-25 10:42:24 +0100

    JI-3114 Register blanks to DOM - Compound content type does not get score until blanks register to DOM# Please enter the commit message for your changes. Lines starting

--- js/blanks.js
+++ js/blanks.js
@@ -122,6 +122,9 @@ H5P.Blanks = (function ($, Question) {
     // Using instructions as label for our text groups
     this.labelId = 'h5p-blanks-instructions-' + Blanks.idCounter;
     this.content = self.createQuestions();
+
+    // Init blanks state
+    this.registerDomElements();
   }
 
   // Inheritance
